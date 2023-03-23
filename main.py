6f699c09c37e9f3df44ecd766876605ea000a256
MuhaFAH <deathserena@yandex.ru>
2023-03-23 20:40:16 +0300

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 import os
 import pygame
 import requests
+import sys
 
-x, y, spn = input('Введите координаты и масштаб (через пробел): ').split()
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&spn={spn},0.002&l=map"
-resp = requests.get(map_request)
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(resp.content)
+
+class MAP:
+    def __init__(self, position):
+        self.name = "map.png"
+        self.pos = position
+        self.spn = 0.01
+        self.diff = 1.5
+        self.response = None
+
+    def update(self, diff):
+        self.spn = self.spn / self.diff if diff == -1 else self.spn * self.diff
+        self.spn = 40 if self.spn >= 40 else round(self.spn, 3)
+        print(self.spn)
+        map_request1 = f'https://geocode-maps.yandex.ru/1.x/'
+        params = {
+            'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
+            'geocode': f'{self.pos}',
+            'format': 'json'
+        }
+        lon, lat = map(str, requests.get(map_request1,
+                                         params=params).json()[
+            "response"][
+            "GeoObjectCollection"][
+            "featureMember"][0][
+            "GeoObject"][
+            "Point"][
+            'pos'].split())
+        map_request2 = f"http://static-maps.yandex.ru/1.x/?ll={lon},{lat}&" \
+                       f"spn={self.spn},{self.spn}&" \
+                       f"l=map&geocode={self.pos}"
+        response = requests.get(map_request2)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request2)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+        self.response = response
+
+    def save(self):
+        with open(self.name, "wb") as file:
+            file.write(self.response.content)
+
+
+map_resp = MAP(input('Введите адрес: '))
+map_resp.update(1)
+map_resp.save()
+
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 pygame.display.set_caption('Большая задача по Maps API')
@@ -18,9 +60,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                pass
-    screen.blit(pygame.image.load(map_file), (0, 0))
+            if event.key == pygame.K_PAGEUP:
+                map_resp.update(+1)
+                map_resp.save()
+            if event.key == pygame.K_PAGEDOWN:
+                map_resp.update(-1)
+                map_resp.save()
+    screen.fill((0, 0, 0))
+    screen.blit(pygame.image.load(map_resp.name), (0, 0))
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
