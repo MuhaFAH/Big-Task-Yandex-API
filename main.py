f240b1612709cb3adf8c78435db665b1cb722781
MuhaFAH <deathserena@yandex.ru>
2023-03-24 13:48:13 +0300

    Большая задача по Maps API. Часть №7

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import pygame_textinput
+import pygame_gui
 import requests
 import sys
 
@@ -41,22 +42,24 @@ class MAP:
         self.response = response
 
     def change_loc(self, location):
-        self.lon, self.lat = map(float,
-                                 requests.get(f'https://geocode-maps.yandex.ru/1.x/',
-                                              params={
-                                                  'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
-                                                  'geocode': f'{location}',
-                                                  'format': 'json'
-                                              }).json()[
-                                     "response"][
-                                     "GeoObjectCollection"][
-                                     "featureMember"][0][
-                                     "GeoObject"][
-                                     "Point"][
-                                     'pos'].split())
-        self.update()
+        try:
+            self.lon, self.lat = map(float,
+                                     requests.get(f'https://geocode-maps.yandex.ru/1.x/',
+                                                  params={
+                                                      'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
+                                                      'geocode': f'{location}',
+                                                      'format': 'json'
+                                                  }).json()[
+                                         "response"][
+                                         "GeoObjectCollection"][
+                                         "featureMember"][0][
+                                         "GeoObject"][
+                                         "Point"][
+                                         'pos'].split())
+            self.update()
+        except (IndexError, KeyError):
+            pass
 
-    # Ничего не изменилось, уже было сделано в №5
     def create_point(self):
         self.point = f'{self.lon},{self.lat}'
         self.update()
@@ -84,7 +87,10 @@ class MAP:
             file.write(self.response.content)
 
 
-map_resp = MAP(input('Введите адрес: '))
+try:
+    map_resp = MAP(input('Введите адрес: '))
+except KeyError:
+    map_resp = MAP('Москва')
 map_resp.change_spn(1)
 map_resp.save()
 
@@ -92,6 +98,17 @@ pygame.init()
 screen = pygame.display.set_mode((600, 450))
 pygame.display.set_caption('Большая задача по Maps API')
 text = pygame_textinput.TextInputVisualizer()
+manager = pygame_gui.UIManager((600, 450))
+button, btn_text, btn_weight, btn_height = [], [['искать'], ['сброс']], 100, 50
+for i in range(len(btn_text)):
+    for j in range(len(btn_text[i])):
+        button.append([])
+        button[i].append(pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((i * btn_weight + 1, (j + 1) * btn_height),
+                                      (btn_weight - 2, btn_height - 2)),
+            text=btn_text[i][j],
+            manager=manager)
+        )
 running = True
 clock = pygame.time.Clock()
 while running:
@@ -99,6 +116,19 @@ while running:
     for event in events:
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            for i in range(len(button)):
+                for j in range(len(button[i])):
+                    if event.ui_element == button[i][j]:
+                        if button[i][j].text == 'искать':
+                            map_resp.change_loc(text.manager.value)
+                            map_resp.create_point()
+                            map_resp.save()
+                        elif button[i][j].text == 'сброс':
+                            map_resp.change_loc('Москва')
+                            text.manager.value = ''
+                            map_resp.create_point()
+                            map_resp.save()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
                 map_resp.change_spn(1)
@@ -122,16 +152,13 @@ while running:
                 map_resp.change_loc(text.manager.value)
                 map_resp.create_point()
                 map_resp.save()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 3:
-                map_resp.change_type()
-                map_resp.save()
-
+        manager.process_events(event)
+    manager.update(clock.tick(60) / 1000.0)
     text.update(events)
     screen.fill((0, 0, 0))
     screen.blit(pygame.image.load(map_resp.name), (0, 0))
     screen.blit(text.surface, (10, 10))
+    manager.draw_ui(screen)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
